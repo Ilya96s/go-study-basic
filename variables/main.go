851440622c8009737c777f7ba6aa0c3b728fa45f
iskaltygin <iskaltygin@fintechiq.ru>
2025-05-04 22:36:12 +0700

    1.Переменные. Создание переменных при помощи короткого вида записи

---
+++ variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 123
+	text := "Text"
+	drob := 1.23
+	boolean := true
+
+	fmt.Println(num)
+	fmt.Println(text)
+	fmt.Println(drob)
+	fmt.Println(boolean)
+}
