489b37dd2a871a1cc67e129fe8c06a0753448966
ilya96s <i.kaltygin@icloud.com>
2025-05-06 23:05:41 +0700

    8. Модули и пакеты. 1 - Простой модуль из одного main пакета

--- variables/main.go
+++ variables/main.go
@@ -1,27 +1,17 @@
 package main
 
-import "fmt"
+import "github.com/k0kubun/pp"
 
 type User struct {
-	Age int
-}
-
-func (u User) ChangeAge1(newAge int) {
-	u.Age = newAge
-}
-
-func (u *User) ChangeAge2(newAge int) {
-	u.Age = newAge
+	Name string
+	Age  int
 }
 
 func main() {
 	user := User{
-		Age: 30,
+		Name: "Василий",
+		Age:  67,
 	}
 
-	fmt.Println("user изначально:", user)
-	user.ChangeAge1(35)
-	fmt.Println("user после ChangeAge1:", user)
-	user.ChangeAge2(35)
-	fmt.Println("user после ChangeAge2:", user)
+	pp.Println(user)
 }
