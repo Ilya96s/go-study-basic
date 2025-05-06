9c2f52d22b50ce8f026f9429d317e82005b7ffa9
ilya96s <i.kaltygin@icloud.com>
2025-05-06 22:53:23 +0700

    7. Структуры. 1 - Структура без методов

--- variables/main.go
+++ variables/main.go
@@ -2,26 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
-	number := 5
-	var notNilPtr *int = &number
-	var nilPtr *int
-
-	fmt.Print("Not nil pointer: ")
-	foo(notNilPtr)
-
-	fmt.Println("")
-
-	fmt.Print("nil pointer: ")
-	foo(nilPtr)
+type User struct {
+	Name        string
+	Age         int
+	PhoneNumber string
+	IsClose     bool
+	Rating      float64
 }
 
-func foo(intPtr *int) {
-	fmt.Println("Указатель:", intPtr)
-
-	if intPtr != nil {
-		fmt.Println("Значение переменной, на которую указывает указатель:", *intPtr)
-	} else {
-		fmt.Println("Указатель является nil-указателем, а значит я не могу его безопасно разыменовать")
+func main() {
+	user1 := User{
+		Name:        "Иван",
+		Age:         50,
+		PhoneNumber: "+7 (911) 911 91-91",
+		IsClose:     true,
+		Rating:      5.5,
 	}
+
+	user2 := User{
+		Name:        "Пётр",
+		Age:         60,
+		PhoneNumber: "+7 (922) 922 92-92",
+		// IsClose не указано => значение по умолчанию (false)
+		// Rating не указано => значение по умолчанию (0.0)
+	}
+
+	fmt.Println("user1:", user1)
+	fmt.Println("user1 Name:", user1.Name)
+
+	fmt.Println("user2:", user2)
+	fmt.Println("user2 Rating:", user2.Rating)
 }
