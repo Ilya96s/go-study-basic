2ca706d011553eb617f67cc49aa00e1e78352b61
ilya96s <i.kaltygin@icloud.com>
2025-05-06 23:13:46 +0700

    9. Массивы, Слайсы, Мапы. 2 - Слайс

--- variables/main.go
+++ variables/main.go
@@ -2,49 +2,105 @@ package main
 
 import (
 	"fmt"
+	"github.com/k0kubun/pp"
 )
 
+type User struct {
+	Name    string
+	Rating  float64
+	Premium bool
+}
+
 func main() {
-	intArr := [6]int{2, 4, 8, 16, 32, 64}
+	foo1()
+	fmt.Println("<<<<<<<>>>>>>>")
+	foo2()
+	fmt.Println("<<<<<<<>>>>>>>")
+	foo3()
+}
 
-	fmt.Println("arr:", intArr)
-
-	// вывод на экран первым видом for
-	for i := 0; i < len(intArr); i++ {
-		fmt.Println(i, "->", intArr[i])
+func foo1() {
+	slice1 := []User{
+		User{
+			Name:    "Иван",
+			Rating:  5.5,
+			Premium: true,
+		},
+		User{
+			Name:    "Сергей",
+			Rating:  4.5,
+			Premium: false,
+		},
+		User{
+			Name:    "Данил",
+			Rating:  7.5,
+			Premium: true,
+		},
 	}
-	fmt.Println("---------------------")
 
-	// умножаем каждый элемент на 2 первый раз первым видом for
-	for i := 0; i < len(intArr); i++ {
-		intArr[i] *= 2
-	}
+	pp.Println("slice1 до:", slice1)
+	fmt.Println("---------------")
 
-	// прибавляет каждому элементу, который больше 60, единицу вторым видом for
-	for index, value := range intArr {
-		if value > 60 {
-			intArr[index] += 1
+	// можем динамически добавлять элементы
+	slice1 = append(
+		slice1, User{
+			Name:    "Олег",
+			Rating:  8.2,
+			Premium: false,
+		},
+	)
+	slice1 = append(
+		slice1, User{
+			Name:    "Пётр",
+			Rating:  3.1,
+			Premium: true,
+		},
+	)
 
-			// value++ не сработало бы
-			// так как value это всего-лишь копия значения из массива
+	pp.Println("slice1 после:", slice1)
+	fmt.Println("---------------")
+}
+
+func foo2() {
+	slice2 := make([]User, 5)
+	pp.Println("slice2:", slice2)
+}
+
+func foo3() {
+	// можем заранее предвыделить память
+	// если знаем кол-во элементов
+	// make([]User, 0, 3)
+	slice3 := make([]User, 0)
+
+	slice3 = append(slice3, User{
+		Name:    "Иван",
+		Rating:  5.5,
+		Premium: true,
+	})
+
+	slice3 = append(slice3,
+		User{
+			Name:    "Сергей",
+			Rating:  4.5,
+			Premium: false,
+		},
+		User{
+			Name:    "Данил",
+			Rating:  7.5,
+			Premium: true,
+		},
+	)
+
+	pp.Println("slice3 до:", slice3)
+	fmt.Println("---------------")
+
+	// каждому у кого Premium аккаун добавляет 1.0 соц рейтинга
+	for index, user := range slice3 {
+		if user.Premium {
+			slice3[index].Rating += 1.0
 		}
 	}
 
-	// вывод на экран вторым видом for
-	for index, value := range intArr {
-		fmt.Println(index, "->", value)
-	}
-	fmt.Println("---------------------")
-
-	// просто вывод всех индексов
-	for index, _ := range intArr {
-		fmt.Println("index:", index)
-	}
-	fmt.Println("---------------------")
-
-	// просто вывод всех значений
-	for _, value := range intArr {
-		fmt.Println("value:", value)
-	}
-	fmt.Println("---------------------")
+	pp.Println("slice3 после:", slice3)
+	fmt.Println("---------------")
 }
