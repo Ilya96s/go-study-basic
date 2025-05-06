71038c5b05096b6a32abfbd9eaf6c26a600261c6
ilya96s <i.kaltygin@icloud.com>
2025-05-06 19:13:35 +0700

    3. Циклыю. 2

--- variables/main.go
+++ variables/main.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	score := 0
+
+	fmt.Println("Get ready")
+	fmt.Println("Score:", score)
+	fmt.Println("")
+
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		fmt.Println("----------------")
+		fmt.Println("Вы подлетаете к трубе", i)
+		fmt.Println("")
+
+		fmt.Println("Вы пролетаете трубу", i)
+		fmt.Println("")
+
+		fmt.Println("Вы пролетели трубу", i)
+		fmt.Println("")
+
+		score++
+
+		fmt.Println("Score:", score)
+		fmt.Println("")
 	}
 }
