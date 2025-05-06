cbc0fcc45c197c50c7a02ec418b155b348161139
ilya96s <i.kaltygin@icloud.com>
2025-05-06 21:56:36 +0700

    5. defer. 1 - Обычный defer

--- variables/main.go
+++ variables/main.go
@@ -2,25 +2,10 @@ package main
 
 import "fmt"
 
-// 10
-var number int = 10
-
 func main() {
-	// 15
-	number += 5
+	defer func() {
+		fmt.Println("Defer Println")
+	}()
 
-	foo()
-	boo()
-
-	fmt.Println("number:", number)
-}
-
-func foo() {
-	// 30
-	number *= 2
-}
-
-func boo() {
-	// 3
-	number /= 10
+	fmt.Println("hello")
 }
