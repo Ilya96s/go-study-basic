fd530c9252729646e1edcd77ac65a034f0d060d5
ilya96s <i.kaltygin@icloud.com>
2025-05-06 18:59:04 +0700

    2. Условные ветвления. 2

--- variables/main.go
+++ variables/main.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	age := 25
 
-	if age > 30 {
+	if age >= 30 {
 		fmt.Println("Age is greater than 30")
+	} else if age <= 12 {
+		fmt.Println("Age is less than 12")
 	} else {
-		fmt.Println("Age is less than 30")
+		fmt.Println("Age is over 12 but under 30")
 	}
 }
