33b93f688b1131ac0bec41ce9833762eede206b1
ilya96s <i.kaltygin@icloud.com>
2025-05-06 21:57:34 +0700

    5. defer. 1 - defer в других функциях

--- variables/main.go
+++ variables/main.go
@@ -4,16 +4,32 @@ import "fmt"
 
 func main() {
 	defer func() {
-		fmt.Println("Defer1")
+		fmt.Println("main Defer1")
+	}()
+	defer func() {
+		fmt.Println("main Defer2")
+	}()
+	defer func() {
+		fmt.Println("main Defer3")
+	}()
+
+	fmt.Println("до вызова foo")
+	foo()
+	fmt.Println("после вызова foo")
+}
+
+func foo() {
+	defer func() {
+		fmt.Println("foo Defer1")
 	}()
 
 	defer func() {
-		fmt.Println("Defer2")
+		fmt.Println("foo Defer2")
 	}()
 
 	defer func() {
-		fmt.Println("Defer3")
+		fmt.Println("foo Defer3")
 	}()
 
-	fmt.Println("hello")
+	fmt.Println("foo")
 }
