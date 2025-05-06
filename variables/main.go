2e64807a57a2b8dea0c34135d233e93d080aaa8b
ilya96s <i.kaltygin@icloud.com>
2025-05-06 19:07:03 +0700

    3. Циклыю. 1

--- variables/main.go
+++ variables/main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	weather := 5
-
-	if weather != +3 {
-		fmt.Println("Сегодня я буду участвовать в гонке!")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
 	}
 }
