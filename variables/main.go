0ad3ee718108f9866dad761db65ce38c072e7076
ilya96s <i.kaltygin@icloud.com>
2025-05-06 23:14:27 +0700

    9. Массивы, Слайсы, Мапы. 3 - Мапа

--- variables/main.go
+++ variables/main.go
@@ -2,105 +2,42 @@ package main
 
 import (
 	"fmt"
-	"github.com/k0kubun/pp"
 )
-
-type User struct {
-	Name    string
-	Rating  float64
-	Premium bool
-}
 
 func main() {
 	foo1()
-	fmt.Println("<<<<<<<>>>>>>>")
+	fmt.Println("-----------")
 	foo2()
-	fmt.Println("<<<<<<<>>>>>>>")
-	foo3()
 }
 
 func foo1() {
-	slice1 := []User{
-		User{
-			Name:    "Иван",
-			Rating:  5.5,
-			Premium: true,
-		},
-		User{
-			Name:    "Сергей",
-			Rating:  4.5,
-			Premium: false,
-		},
-		User{
-			Name:    "Данил",
-			Rating:  7.5,
-			Premium: true,
-		},
+	weather := map[int]int{
+		10: +3,
+		11: -4,
+		12: +1,
 	}
 
-	pp.Println("slice1 до:", slice1)
-	fmt.Println("---------------")
+	weather[12] = -10
+	weather[13] = +5
 
-	// можем динамически добавлять элементы
-	slice1 = append(
-		slice1, User{
-			Name:    "Олег",
-			Rating:  8.2,
-			Premium: false,
-		},
-	)
-	slice1 = append(
-		slice1, User{
-			Name:    "Пётр",
-			Rating:  3.1,
-			Premium: true,
-		},
-	)
+	for key, value := range weather {
+		fmt.Println("key:", key, ", value:", value)
+	}
 
-	pp.Println("slice1 после:", slice1)
-	fmt.Println("---------------")
+	w11 := weather[11]
+	fmt.Println("погода 11-го числа:", w11)
 }
 
 func foo2() {
-	slice2 := make([]User, 5)
-	pp.Println("slice2:", slice2)
-}
+	weather := make(map[int]int)
 
-func foo3() {
-	// можем заранее предвыделить память
-	// если знаем кол-во элементов
-	// make([]User, 0, 3)
-	slice3 := make([]User, 0)
+	weather[10] = -2
+	weather[11] = -1
+	weather[12] = 0
 
-	slice3 = append(slice3, User{
-		Name:    "Иван",
-		Rating:  5.5,
-		Premium: true,
-	})
+	w12, ok12 := weather[12]
+	w13, ok13 := weather[13]
 
-	slice3 = append(slice3,
-		User{
-			Name:    "Сергей",
-			Rating:  4.5,
-			Premium: false,
-		},
-		User{
-			Name:    "Данил",
-			Rating:  7.5,
-			Premium: true,
-		},
-	)
-
-	pp.Println("slice3 до:", slice3)
-	fmt.Println("---------------")
-
-	// каждому у кого Premium аккаун добавляет 1.0 соц рейтинга
-	for index, user := range slice3 {
-		if user.Premium {
-			slice3[index].Rating += 1.0
-		}
-	}
-
-	pp.Println("slice3 после:", slice3)
-	fmt.Println("---------------")
+	fmt.Println("погода 12-го числа:", w12, ", является ли это записанным ранее значением, а не значением по-умолчанию:", ok12)
+	fmt.Println("погода 13-го числа:", w13, ", является ли это записанным ранее значением, а не значением по-умолчанию:", ok13)
 }
