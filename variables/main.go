08566826a1f930738db6a88692eb225c9a492193
ilya96s <i.kaltygin@icloud.com>
2025-05-06 21:57:09 +0700

    5. defer. 1 - Множественный defer

--- variables/main.go
+++ variables/main.go
@@ -4,7 +4,15 @@ import "fmt"
 
 func main() {
 	defer func() {
-		fmt.Println("Defer Println")
+		fmt.Println("Defer1")
+	}()
+
+	defer func() {
+		fmt.Println("Defer2")
+	}()
+
+	defer func() {
+		fmt.Println("Defer3")
 	}()
 
 	fmt.Println("hello")
