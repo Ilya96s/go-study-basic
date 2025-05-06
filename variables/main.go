271ca8efe3118bbee70161e03442763ecde2906d
ilya96s <i.kaltygin@icloud.com>
2025-05-06 23:12:19 +0700

    9. Массивы, Слайсы, Мапы. 1 - Массив

--- variables/main.go
+++ variables/main.go
@@ -2,24 +2,49 @@ package main
 
 import (
 	"fmt"
-	"github.com/k0kubun/pp"
-	"study/greeting"
-	"study/user"
 )
 
 func main() {
-	greeting.SayHello()
-	i := greeting.GiveMeInt()
-	fmt.Println("i:", i)
+	intArr := [6]int{2, 4, 8, 16, 32, 64}
 
-	u := user.User{
-		Name: "Григорий",
-		// age не видно, потому что age private
+	fmt.Println("arr:", intArr)
+
+	// вывод на экран первым видом for
+	for i := 0; i < len(intArr); i++ {
+		fmt.Println(i, "->", intArr[i])
+	}
+	fmt.Println("---------------------")
+
+	// умножаем каждый элемент на 2 первый раз первым видом for
+	for i := 0; i < len(intArr); i++ {
+		intArr[i] *= 2
 	}
 
-	u.ChangeAge(50)
-	// changeName не видно, потому что changeName private
-	// user.changeName
+	// прибавляет каждому элементу, который больше 60, единицу вторым видом for
+	for index, value := range intArr {
+		if value > 60 {
+			intArr[index] += 1
 
-	pp.Println("user:", u)
+			// value++ не сработало бы
+			// так как value это всего-лишь копия значения из массива
+		}
+	}
+
+	// вывод на экран вторым видом for
+	for index, value := range intArr {
+		fmt.Println(index, "->", value)
+	}
+	fmt.Println("---------------------")
+
+	// просто вывод всех индексов
+	for index, _ := range intArr {
+		fmt.Println("index:", index)
+	}
+	fmt.Println("---------------------")
+
+	// просто вывод всех значений
+	for _, value := range intArr {
+		fmt.Println("value:", value)
+	}
+	fmt.Println("---------------------")
 }
