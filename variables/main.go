61aec1126b412441640466225f775bbc78fe35eb
ilya96s <i.kaltygin@icloud.com>
2025-05-06 19:20:42 +0700

    4. Функции. 1

--- variables/main.go
+++ variables/main.go
@@ -1,46 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	score := 0
-
-	fmt.Println("Get Ready")
-	fmt.Println("Счёт:", score)
-	fmt.Println("")
-
-	// Бесконечный цикл (аналог while true в других языках)
-	for {
-		fmt.Println("---------------------")
-		fmt.Println("Я подлетаю к трубе!")
-		fmt.Println("🐤 🔋🔋")
-		fmt.Println("")
-
-		fmt.Println("Я пролетаю через трубу!")
-		fmt.Println("🔋🐤🔋")
-		fmt.Println("")
-
-		if rand.Intn(8) == 1 {
-			fmt.Println("Я врезался в трубу :(")
-			fmt.Println("🔋❌🔋")
-
-			// прерывание цикла
-			break
-		}
-
-		fmt.Println("Я пролетел через трубу!")
-		fmt.Println("🔋🔋 🐤")
-		fmt.Println("")
-
-		score++
-
-		fmt.Println("Счёт:", score)
-		fmt.Println("")
-
-		time.Sleep(500 * time.Millisecond)
-	}
+	fmt.Println("Сейчас начнется вызов функции sayHello()")
+	sayHello()
+}
+func sayHello() {
+	fmt.Println("Hello World")
 }
