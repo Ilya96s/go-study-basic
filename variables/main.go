2ec925647092abf2b37bfa6b9b23f8d119a8c352
ilya96s <i.kaltygin@icloud.com>
2025-05-06 18:56:40 +0700

    2. Условные ветвления. 1

--- variables/main.go
+++ variables/main.go
@@ -3,36 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	number := 15
-	text := "Hello"
-	drob := 11.25
-	boolean := true
+	age := 25
 
-	// сложение
-	number = number + 10
-	number += 15
-	number++
-
-	// умножение
-	number = number * 100
-	number *= 5
-
-	// деление
-	number = number / 10
-	number /= 5
-
-	// вычитание
-	drob = drob - 1.05
-	drob -= 4.2
-	drob--
-
-	// конкатенация
-	text = text + "World"
-	text += "!"
-
-	// вывод в консоль
-	fmt.Println("number:", number)
-	fmt.Println("text:", text)
-	fmt.Println("drob:", drob)
-	fmt.Println("boolean:", boolean)
+	if age > 30 {
+		fmt.Println("Age is greater than 30")
+	} else {
+		fmt.Println("Age is less than 30")
+	}
 }
